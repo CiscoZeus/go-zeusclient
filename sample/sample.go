1bd57c27e866f1cddb362f01b1d22341a30ad9bf
kosmori <kosmori@cisco.com>
2016-10-26 17:29:50 -0700

    Add functions for API of Alert and Trigalert

--- sample/sample.go
+++ sample/sample.go
@@ -21,7 +21,64 @@ func randString(n int) string {
 }
 
 func main() {
-	zeus := &Zeus{ApiServ: "http://api.ciscozeus.io", Token: "{Your token}"}
+	zeus := &Zeus{ApiServ: "https://api.ciscozeus.io", Token: "{Your token}"}
+
+	postAlert := Alert{
+		Alert_name:	randString(5),
+		Username:	randString(5),
+		Token: "databucket",
+		Alerts_type: "metric",
+		Alert_expression: "cpu.value > 20",
+		Alert_severity: "S1",
+		Metric_name: "cpu.value",
+		Emails: "blah@blah.com",
+		Status: "active",
+		Frequency: 30.0,
+	}
+	fmt.Printf("Test Alerts API with alert_name: %s, username : %s\n", postAlert.Alert_name, postAlert.Username)
+	status, err := zeus.PostAlert(postAlert)
+	fmt.Println("PostAlert: ", status, err)
+	if err != nil {
+		fmt.Println("=> Failed")
+	}
+
+	total, alerts, err := zeus.GetAlerts("")
+	fmt.Println("GetAlerts: ", total, alerts, err)
+	if err != nil {
+		fmt.Println("=> Failed")
+	}
+	postId := alerts[len(alerts)-1].Id
+	fmt.Printf("(postId: %d)\n", postId)
+
+	putAlert := Alert{
+		Alert_name: randString(5),
+		Username: randString(5),
+		Token: "databucket",
+		Alerts_type: "metric",
+		Alert_expression: "cpu.value > 20",
+		Alert_severity: "S1",
+		Metric_name: "cpu.value",
+		Emails: "blah@blah.com",
+		Status: "active",
+		Frequency: 32.0,
+	}
+	status, err = zeus.PutAlert(postId, putAlert)
+	if err != nil {
+		fmt.Println("=> Failed")
+	}
+	fmt.Println("PostAlert: ", status, err)
+	fmt.Printf("The Alert is changed to alert_name: %s, username : %s\n", putAlert.Alert_name, putAlert.Username)
+
+	alert, err := zeus.GetAlert(postId)
+	if err != nil {
+		fmt.Println("=> Failed")
+	}
+	fmt.Println("GetAlert: ", alert, err)
+
+	status, err = zeus.EnableAlerts([]int64{postId})
+	fmt.Println("EnableAlerts: ", status, err)
+	status, err = zeus.DisableAlerts([]int64{postId})
+	fmt.Println("DisableAlerts: ", status, err)
 
 	logs := LogList{
 		Name: randString(5),
@@ -69,6 +126,7 @@ func main() {
 	}
 	fmt.Printf("sent 2 metrics, %d successful\n", suc)
 
+
 	fmt.Println("\nsleep for 20 seconds")
 	time.Sleep(20 * time.Second)
 	name, err := zeus.GetMetricNames(metrics.Name, 0, 0)
@@ -89,4 +147,16 @@ func main() {
 		panic("failed to delete one series")
 	}
 	fmt.Println("deleted")
+
+	trigalert, err := zeus.GetTrigalert()
+	fmt.Println("GetTrigalert: ", trigalert, err)
+	if err != nil {
+		fmt.Println("=> Failed")
+	}
+
+	trigalert, err = zeus.GetTrigalertLast24()
+	fmt.Println("GetTrigalertLast24: ", trigalert, err)
+	if err != nil {
+		fmt.Println("=> Failed")
+	}
 }
